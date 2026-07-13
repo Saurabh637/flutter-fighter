31388d2db60d6b2c9e9f1f5215735e96698bbe81
Saurabh637 <saurabharanjan2004@gmail.com>
2026-07-13 12:44:29 +0530

    Implement player gravity system

--- lib/components/player/player.dart
+++ lib/components/player/player.dart
@@ -9,12 +9,33 @@ class Player extends RectangleComponent with HasGameRef {
   /// The fixed size of the player character.
   static final Vector2 playerSize = Vector2(50, 100);
 
+  /// The vertical velocity of the player (pixels per second).
+  double _verticalVelocity = 0.0;
+
+  /// The gravity constant (acceleration in pixels per second squared).
+  /// This value determines how fast the player accelerates downward.
+  static const double _gravity = 800.0;
+
   Player()
       : super(
           size: playerSize,
           paint: Paint()..color = const Color(0xFF2196F3), // Blue color
           anchor: Anchor.bottomCenter, // Anchor to bottom for easy floor placement
         );
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+
+    // 1. Apply gravity to vertical velocity.
+    // velocity = initial_velocity + acceleration * time
+    _verticalVelocity += _gravity * dt;
+
+    // 2. Apply vertical velocity to position.
+    // position = initial_position + velocity * time
+    // We update position.y directly.
+    position.y += _verticalVelocity * dt;
+  }
 
   @override
   Future<void> onLoad() async {
